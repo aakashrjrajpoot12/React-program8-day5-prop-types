75705932ef69f3962b62c62c1d7ec10049e286e2
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-18 12:39:50 +0530

    1 file modified

--- src/Steos/UserComponent.js
+++ src/Steos/UserComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 export default class UserComponent extends React.Component{
   render(){
     return(
@@ -11,6 +12,26 @@ export default class UserComponent extends React.Component{
       <p>Age: {this.props.age}</p>
 
       </>
-    ):
+    );
   }
 }
+
+UserComponent.PropTypes={
+  name:PropTypes.string.isRequired,
+  projects:PropTypes.array,
+  age(props,propName){
+    if(typeof props[propName]!=='number'){
+      return new Error('Age must be a number');
+     }
+     if(props[propName]>100){
+       return new Error('Age entered is'+props[propName]+', it should be less than or equal to 100'
+       );
+     }
+
+  },
+};
+/*
+UserComponent.defaultProps={
+  name:'Vinay',
+};
+*/
